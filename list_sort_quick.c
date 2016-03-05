c62da7736eca5d620f709e0bbd873e34a542de17
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-03-05 00:24:29 -0500

    Initial commit for 148 list sort using quick sort

---
+++ list_sort_quick.c
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+static inline struct ListNode *lastElement(struct ListNode *head) 
+{
+    if (!head) return NULL;
+    
+    while (head->next)
+        head = head->next;
+
+    return head;
+}
+
+struct ListNode* sortList(struct ListNode* head) {
+    struct ListNode *se_head, *l_head, *pvt;
+    struct ListNode *curr, prv, nxt;
+
+    if (!head) return NULL;
+    if (!head->next) return head;
+
+    se_head = NULL;
+    l_head = NULL;
+    pvt   = head;
+    curr  = head->next;
+    while (curr) {
+        nxt = curr->next;
+
+        if (curr->val > pvt->val) {
+            curr->next = l_head;
+            l_head = curr;
+        }
+        else {
+            curr->next = se_head;
+            se_head = curr;
+        }
+
+        curr = nxt;
+    }
+
+    se_head = sortList(se_head);
+    l_head  = sortList(l_head);
+    
+    prv = lastElement(se_head);
+    if (!prv)
+        se_head   = pvt;
+    else
+        prv->next = pvt;
+
+    pvt->next = l_head;
+
+    
+    return se_head;
+}
