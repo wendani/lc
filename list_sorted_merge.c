2d986a7df57cae18c7197b3621625e3185539ac8
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-03-06 12:34:10 -0500

    Fix bug: Missing one & for logical AND operation

--- list_sorted_merge.c
+++ list_sorted_merge.c
@@ -21,7 +21,7 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
         tail = l2;
         l2 = l2->next;
     }
-    while (l1 & l2) {
+    while (l1 && l2) {
         if (l1->val < l2->val) {
             tail->next = l1;
             l1 = l1->next;
