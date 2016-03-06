6debbff7e3c71cf3ac084824d9e4e683d4d6bb78
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-03-06 00:39:55 -0500

    Initial commit of 142 return the node where linked list cycle begins

---
+++ list_cycle_2.c
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    struct ListNode *slow, *fast;
+
+    if (!head) return NULL;
+
+    fast = head->next;
+    if (!fast) return NULL;
+    fast = fast->next;
+    if (!fast) return NULL; 
+
+    slow = head;
+    while (slow != fast) {
+        fast = fast->next;
+        if (!fast) return NULL;
+        fast = fast->next; 
+        if (!fast) return NULL; 
+        
+        slow = slow->next;
+    }
+
+    while (head != fast) {
+        head = head->next;
+        fast = fast->next;
+    }
+    return fast;
+}
