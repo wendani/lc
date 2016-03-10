68bb1d4280839a26dd7b3888ec4f1ab125ffa774
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-03-10 09:22:54 -0500

    Conisder another case where numsSize % k == 0, but this is not enough
    The algorithm idea itself is not general

--- array_rotate.c
+++ array_rotate.c
@@ -1,12 +1,33 @@
 void rotate(int* nums, int numsSize, int k) {
-    int oidx. nidx;
-    int i, otemp, ntemp;
+    int oidx, nidx, otemp, ntemp;
+    int i, j, cnt;
 
     if (k % numsSize == 0) return;
 
     oidx = 0;
     otemp = nums[oidx];
-    for (i = 1; i <= n; ++i) {
+
+    if (numsSize % k == 0) {
+        cnt = numsSize / k;
+        for (i = 1; i <= k; ++i) {
+            for (j = 1; j <= cnt; ++j) {
+                nidx = oidx + k;
+                if (j == cnt)
+                    nidx -= numsSize;
+
+                ntemp = nums[nidx];
+                nums[nidx] = otemp;
+
+                oidx = nidx;
+                otemp = ntemp;
+            }
+            ++oidx;
+            otemp = nums[oidx];
+        }
+        return;
+    }
+
+    for (i = 1; i <= numsSize; ++i) {
         nidx = (oidx + k) % numsSize;
         
         ntemp = nums[nidx];
@@ -15,5 +36,4 @@ void rotate(int* nums, int numsSize, int k) {
         oidx = nidx;
         otemp = ntemp;
     }
-        
 }
