752ac5d7c1e4236425db9db193462d29c61576a6
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-03-12 19:23:55 -0500

    Initial commit of 104 finding the maximum depth of binary tree---Pass!

---
+++ tree_binary_max_depth.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    int ldepth, rdepth;
+
+    if (!root) 
+        return 0;
+
+    ldepth = maxDepth(root->left);
+    rdepth = maxDepth(root->right);
+
+    if (ldepth < rdepth)
+        return rdepth + 1;
+
+    return ldepth + 1;
+}
