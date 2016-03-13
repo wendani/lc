fdb07f9a4f8fcbd3bbba1be1a4c83c6b8d6653cf
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-03-12 20:02:23 -0500

    Initial commit of 111 finding minimum depth of binary tree

---
+++ tree_binary_min_depth.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int minDepth(struct TreeNode* root) {
+    int ldepth, rdepth;
+
+    if (!root)
+        return 0;
+
+    ldepth = minDepth(root->left);
+    rdepth = minDepth(root->right);
+
+    if (ldepth > rdepth)
+        return rdepth + 1;
+
+    return ldepth + 1;
+}
