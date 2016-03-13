a8537496a6beb1b28219aa276f73ed49a5a573e0
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-03-12 20:24:40 -0500

    leaf node must have both left and right pointers to be NULL

--- tree_binary_min_depth.c
+++ tree_binary_min_depth.c
@@ -15,6 +15,13 @@ int minDepth(struct TreeNode* root) {
     ldepth = minDepth(root->left);
     rdepth = minDepth(root->right);
 
+    if (!ldepth)
+        return rdepth + 1;
+
+    if (!rdepth)
+        return ldepth + 1;
+
+    // reach here if both ldepth and rdepth are non-zero
     if (ldepth > rdepth)
         return rdepth + 1;
 
