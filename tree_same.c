6055cce6f185136f117fa0eeb0f08bda0e64d1c6
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-03-13 00:45:29 -0500

    Initial commit of 100 check if two binary trees are equal or not

---
+++ tree_same.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (!p && !q) 
+        return true;
+    if ((!p && q) || (p && !q))
+        return false;
+    
+    // reach here if (p && q)
+    if (p->val != q->val)
+        return false;
+
+    if (!isSameTree(p->left, q->left))
+        return false;
+
+    if (!isSameTree(p->right, q->right)
+        return false;
+
+    return true;
+}
