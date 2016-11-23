3b84070bdfcd7e95bb2620adea6f5e6aa5150965
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-11-23 17:33:58 -0500

    Initial commit of Q453 Minimum moves to equal array elements
     Changes to be committed:
    	new file:   array_min_move_2_equal.c

---
+++ array_min_move_2_equal.c
@@ -0,0 +1,65 @@
+#define EINVAL 22
+
+/**
+ * return 1, if all elements are equal;
+ * 0, otherwise
+ */
+static int is_equal(int *nums, int num_size)
+{
+	int flag = 1;
+	int i;
+
+	if (!nums || !num_size)
+		return flag;
+
+	// Reach here to contain at least one element
+	for (i = 1; i < num_size; ++i)
+		if (nums[i - 1] != nums[i]) {
+			flag = 0;
+			break;
+		}
+
+	return flag;
+}
+
+/**
+ * return the index of the maximum element
+ * -1, if array is NULL
+ */
+static int get_max(int *nums, int num_size)
+{
+	int indx = 0;
+
+	if (!nums || !num_size)
+		return -1;
+
+	// Reach here to contain at least one element
+	for (i = 1; i < num_size; ++i)
+		if (nums[i] > nums[indx])
+			indx = i;
+
+	return indx;
+}
+
+int minMoves(int* nums, int numsSize) {
+	int moves;
+
+	if (!nums || !numsSize)
+		return 0;
+
+	moves = 0;
+	while (!is_equal(nums, numsSize)) {
+		indx = get_max(nums, numsSize);
+		if (indx < 0)
+			return -EINVAL;
+
+		for (i = 0; i < numsSize; i++) {
+			if (i == indx)
+				continue;
+			++nums[i];
+		}
+		++moves;
+	}
+
+	return  moves;
+}
