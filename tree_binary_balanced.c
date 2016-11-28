f58328158ba996164de73bdd9a4cff07f60e4412
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2016-11-27 20:53:31 -0500

    Initial commit of Q110 Balanced binary tree
     Changes to be committed:
    	new file:   tree_binary_balanced.c

---
+++ tree_binary_balanced.c
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+static bool __isBalanced(struct TreeNode *root, int *depth)
+{
+	int ldepth, rdepth;
+	bool lbalanced, rbalanced;
+
+	if (!root) {
+		if (depth)
+			*depth = 0;
+		return true;
+	}
+
+	lbalanced = __isBalanced(root->left, &ldepth);
+	rbalanced = __isBalanced(root->right, &rdepth);
+	if (depth)
+		*depth = (*ldepth >= *rdepth) ? (*ldepth + 1) : (*rdepth + 1);
+
+	if (!lbalanced || !rbalanced) {
+		return false;
+	}
+
+	// reach here when both children are balanced
+	return (abs(*ldepth - *rdepth) > 1) ? false : true;
+}
+
+bool isBalanced(struct TreeNode* root) {
+	bool balanced;
+
+	balanced = __isBalanced(root, NULL);
+	return balanced;
+}
