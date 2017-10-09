6b5393205eda3ccc2619b7cd5a372fede8d0ae4f
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2017-10-09 14:20:07 -0400

    First implementation
    	new file:   tree_binary_build_pre_in_order.c

---
+++ tree_binary_build_pre_in_order.c
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int inorderSize) {
+	int rval;
+	struct TreeNode *root, *ltree, *rtree;
+
+	if ((!preorder) || (!inorder))
+		return NULL;
+
+	if ((preorderSize < 1) || (inorderSize < 1))
+		return NULL;
+
+	rval = preorder[0];
+	for (i = 0; inorderSize[i] != rval; i++)
+	assert(i < inorderSize);
+
+	ltree = buildTree(preorder + 1, i, inorder, i);
+	rtree = buildTree(preorder + 1 + i, preorderSize - i - 1, inorder + i + 1, inorderSize - i - 1);
+
+	root = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+	root->val = rval;
+	root->left = ltree;
+	root->right = rtree;
+	return root;
+}
+
