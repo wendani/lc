7105bb3b49ee1b144bc48594108eba58ca7ff01d
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2017-10-19 09:01:26 -0400

    Revisit the concept of binary search tree
    Correction to the first design

--- tree_binary_search_validate.c
+++ tree_binary_search_validate.c
@@ -6,18 +6,32 @@
  *     struct TreeNode *right;
  * };
  */
-bool isValidBST(struct TreeNode* root) {
+bool __isValidBST(struct TreeNode* root, int *lower_bound_for_left_child, int *upper_bound_for_right_child) {
 	if (!root)
 		return true;
 
-	if ((root->left) && (root->left->val >= root->val))
-		return false;
+	if (root->left) {
+		if (root->left->val >= root->val)
+			return false;
 
-	if ((root->right) && (root->right->val <= root->val))
-		return false;
+		if ((lower_bound_for_left_child) && (root->left->val <= *lower_bound_for_left_child)
+			return false;
+	}
 
-	if (isValidBST(root->left))
-		return isValidBST(root->right);
+	if (root->right) {
+		if (root->right->val <= root->val)
+			return false;
+
+		if ((upper_bound_for_right_child) && (root->right->val >= *upper_bound_for_right_child))
+			return false;
+	}
+
+	if (__isValidBST(root->left, lower_bound_for_left_child, &root->val))
+		return __isValidBST(root->right, &root->val, upper_bound_for_right_child);
 	else
 		return false;
+}
+
+bool isValidBST(struct TreeNode* root) {
+	return __isValidBST(root, NULL, NULL);
 }
