2edbe0f63dfd9189b172964c9ace6b9da632855c
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2017-10-19 13:05:17 -0400

    Implementation improvement
    	modified:   tree_binary_search_validate.c

--- tree_binary_search_validate.c
+++ tree_binary_search_validate.c
@@ -6,6 +6,27 @@
  *     struct TreeNode *right;
  * };
  */
+bool __isValidBST(struct TreeNode* root, int *lower, int *upper) {
+	if (!root)
+		return true;
+
+	if ((lower) && (root->val <= *lower))
+		return false;
+
+	if ((upper) && (root->val >= *upper))
+		return false;
+
+	if (__isValidBST(root->left, lower, &root->val))
+		return __isValidBST(root->right, &root->val, upper);
+	else
+		return false;
+}
+
+bool isValidBST(struct TreeNode* root) {
+	return __isValidBST(root, NULL, NULL);
+}
+
+
 bool __isValidBST(struct TreeNode* root, int *lower_bound_for_left_child, int *upper_bound_for_right_child) {
 	if (!root)
 		return true;
