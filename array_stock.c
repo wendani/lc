29245919755004f29855eed5f44e3eb80ca7ca1c
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-06-23 16:38:59 -0700

    Correct implementation
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- array_stock.c
+++ array_stock.c
@@ -1,57 +1,19 @@
 int maxProfit(int* prices, int pricesSize){
-	int i, j;
-	int min_idx, max_idx, max_prof;
-	int head, tail;
+	int i;
+	int head, max_prof;
 
 	if (!prices || pricesSize < 2)
 		return 0;
 
 	head = 0;
-	tail = pricesSize - 1;
-	min_idx = head;
-	max_idx = tail;
-	max_prof = prices[max_idx] - prices[min_idx];
-	if (max_prof < 0)
-		max_prof = 0;
-
-	i = head;
-	j = tail;
-	while (i < j) {
-		i++;
-		j--;
-
-		if (prices[head] >= prices[i]) {
+	max_prof = 0;
+	for (i = 1; i < pricesSize; i++) {
+		if (prices[head] >= prices[i])
 			head = i;
-			if (prices[tail] - prices[head] > max_prof) {
-				min_idx = head;
-				max_idx = tail;
-				max_prof = prices[tail] - prices[head];
-			}
-		}
 		else {
 			// prices[head] < prices[i]
-			if (prices[i] - prices[head] > max_prof) {
-				min_idx = head;
-				max_idx = i;
+			if (prices[i] - prices[head] > max_prof)
 				max_prof = prices[i] - prices[head];
-			}
-		}
-
-		if (prices[j] >= prices[tail]) {
-			tail = j;
-			if (prices[tail] - prices[head] > max_prof) {
-				min_idx = head;
-				max_idx = tail;
-				max_prof = prices[tail] - prices[head];
-			}
-		}
-		else {
-			// prices[j] < prices[tail]
-			if (prices[tail] - prices[j] > max_prof) {
-				min_idx = j;
-				max_idx = tail;
-				max_prof = prices[j] - prices[tail];
-			}
 		}
 	}
 
