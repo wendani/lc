3dfa086dac5c4054208a6d77414e7b9f60872875
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-06-24 21:03:59 -0700

    Add base condition Add strlen check for word in dictionary
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- string_word_break.c
+++ string_word_break.c
@@ -1,13 +1,21 @@
 bool wordBreak(char * s, char ** wordDict, int wordDictSize){
-	int i;
-	size_t len = strlen(s);
+	int i, j;
+	size_t len;
+
+	if (!s)
+		return false;
+
+	len = strlen(s);
+	if (!len)
+		return true;
 
 	for (i = 1; i <= len; i++)
 		for (j = 0; j < wordDictSize; j++)
-			if (!strncmp(s, wordDict[j], i) &&
-			    wordBreak(&s[i], wordDict, wordDictSize))
+			if (strlen(wordDict[j]) == i && !strncmp(s, wordDict[j], i) &&
+			    wordBreak(&s[i], wordDict, wordDictSize)) {
 				printf("%s\n", &s[i]);
 				return true;
+			}
 
 	return false;
 }
