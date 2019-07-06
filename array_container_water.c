2f601946392af0a38938c4da123f8227b3047e38
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-06 16:56:15 -0700

    On branch 11_array_container_with_most_water, new implementation using invariant approach
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- array_container_water.c
+++ array_container_water.c
@@ -1,5 +1,51 @@
 int maxArea(int* height, int heightSize) {
 	int i, j;
+	int lo, hi;
+	int hasUpdate;
+
+	lo = 0;
+	hi = heightSize - 1;
+	maxWater = (hi - lo) * (height[lo] <= height[hi] ? height[lo] : height[hi]);
+	i = lo;
+	j = hi;
+	while (i < j) {
+		int water = -1;
+		int hasUpdate = 0;
+		if (height[lo] < height[i]) {
+			lo = i;
+			hasUpdate = 1;
+		}
+		else if (height[j] > height[hi]) {
+			hi = j;
+			hasUpdate = 1;
+		}
+
+		if (height[lo] <= height[hi]) {
+			i++;
+			if (hasUpdate) {
+				water = (hi - lo) * height[lo];
+			}
+		}
+		else {
+			// height[lo] > height[hi]
+			j--;
+			if (hasUpdate) {
+				water = (hi - lo) * height[hi];
+			}
+		}
+		if (hasUpdate && water > maxWater) {
+			maxWater = water;
+		}
+	}
+
+	return maxWater;
+}
+
+
+
+
+int maxArea(int* height, int heightSize) {
+	int i, j;
 	int max_water;
 
 	max_water = 0;
