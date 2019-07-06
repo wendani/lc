36bcbc669c88a913a13dd709de8188856ef6e26d
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-05 23:32:38 -0700

    On branch 15_3Sum, fix logic: use sort approach
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- vector_3sum.cpp
+++ vector_3sum.cpp
@@ -1,18 +1,34 @@
 vector<vector<int>> threeSum(vector<int>& nums) {
-	unordered_map<int, int> numIdxMap;
-	int len = nums.size();
-	for (int i = 0; i < len; i++) {
-		numIdxMap.emplace(nums[i], i);
-	}
+	sort(nums.begin(), nums.end());
 
 	vector<vector<int>> results;
-	for (int i = 0; i < len - 2; i++)
-		for (int j = i + 1; j < len - 1; j++) {
-			auto it = numIdxMap.find(0 - nums[i] - nums[j]);
-			if (it != numIdxMap.end() && j < it->second) {
-				results.emplace_back(initializer_list<int>({nums[i], nums[j], it->first}));
+	int len = nums.size();
+	for (int i = 0; i < len - 2; i++) {
+		if (i > 0 || nums[i] == nums[i - 1]) {
+			continue;
+		}
+
+		int t = 0 - nums[i];
+		int j = i + 1;
+		int k = len - 1;
+		while (j < k) {
+			if (nums[j] + nums[k] < t) {
+				j++;
+			}
+			else if (nums[j] + nums[k] > t) {
+				k--;
+			}
+			else {
+				// nums[j] + num[k] == t
+				if ((j == i + 1) || (k == len - 1)
+					|| (nums[j] != nums[j -1]) || nums[k] != nums[k - 1]) {
+					results.emplace_back(initializer_list<int>({nums[i], nums[j], nums[k]}));
+				}
+				j++;
+				k--;
 			}
 		}
+	}
 
 	return results;
 }
