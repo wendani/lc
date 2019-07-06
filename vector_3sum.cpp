afa5497abdc7971f5b1793c6f942f45ac6b8425f
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-05 20:10:52 -0700

    On branch 15_3Sum 	new file:   vector_3sum.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ vector_3sum.cpp
@@ -0,0 +1,18 @@
+vector<vector<int>> threeSum(vector<int>& nums) {
+	unordered_map<int, int> numIdxMap;
+	int len = nums.size();
+	for (int i = 0; i < len; i++) {
+		numIdxMap.emplace(nums[i], i);
+	}
+
+	vector<vector<int>> results;
+	for (int i = 0; i < len - 2; i++)
+		for (int j = i + 1; j < len - 1; j++) {
+			auto it = numIdxMap.find(0 - nums[i] - nums[j]);
+			if (it != numIdxMap.end() && it->second > j) {
+				results.emplace_back(initializer_list<int>({a[i], a[j], it->first}));
+			}
+		}
+
+	return results;
+}
