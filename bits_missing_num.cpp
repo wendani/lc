204833ad0799d7d5aac773968860c2a1c0337bea
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-07 13:44:58 -0700

    On branch 268_bits_missing_umber, fix compile error: bitset does not allow runtime size; using vector<bool>
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- bits_missing_num.cpp
+++ bits_missing_num.cpp
@@ -1,5 +1,22 @@
 int missingNumber(vector<int>& nums) {
 	int size = nums.size() + 1;
+	vector<bool> bitmap(size, false);
+
+	for (const auto &num : nums) {
+		bitmap[num] = true;
+	}
+
+	for (int i = 0; i < size; i++) {
+		if (!bitmap[i])
+			return i;
+	}
+
+	return -1;
+}
+
+
+int missingNumber(vector<int>& nums) {
+	int size = nums.size() + 1;
 	bitset<size> bs;
 	for (const auto &num : nums) {
 		bs.set(num);
