fe73e43c19ae97700b3eabd06c051df6869fb621
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-07 17:29:49 -0700

    On branch 20_string_valid_parentheses 	new file:   string_parentheses_valid.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ string_parentheses_valid.cpp
@@ -0,0 +1,38 @@
+static const unordered_map<char, char> bracketPair;
+static const unordered_set<char> openBrackets;
+
+bool isValid(string s) {
+	stack<char> cStk;
+
+	for (const auto &c : s) {
+		if (openBrackets.count(c)) {
+			cStk.push(c);
+		}
+		else {
+			// closing bracket
+			if (!cStk.empty()) {
+				char oc = cStk.top();
+				if (bracketPair.at(oc) != c) {
+					return false;
+				}
+				// reach here when oc == c
+				cStk.pop();
+			}
+			else {
+				// cStk is empty; does not contain
+				// any open bracket for pairing
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+const unordered_map<char, char> Solution::bracketPair = {
+	{'(', ')'},
+	{'[', ']'},
+	{'{', '}'}
+};
+
+const unordered_set<char> Solution::openBrackets = {'(', '[', '{'};
