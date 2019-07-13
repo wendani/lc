d0a7684c7506727239b3f507fc1a5aa64e74acc0
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-13 13:33:03 -0700

    On branch 763_string_partition_labels new file:   string_partition_labels.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ string_partition_labels.cpp
@@ -0,0 +1,29 @@
+vector<int> partitionLabels(string S) {
+	int len = S.length();
+
+	unordered_map<char, int> charLastIdx;
+	int i;
+	for (i = 0; i < len; i++) {
+		charLastIdx[S[i]] = i;
+	}
+
+	vector<int> partitions;
+	i = 0;
+	int prevLast = -1;
+	while (i < len) {
+		int currLast = charLastIdx[S[i]];
+
+		while (i < currLast) {
+			if (charLastIdx[S[i]] > currLast) {
+				currLast = charLastIdx[S[i]];
+			}
+			i++;
+		}
+
+		// reach here when i == currLast
+		partitions.push_back(currLast - prevLast);
+		prevLast = currLast;
+		i++;
+	}
+	return partitions;
+}
