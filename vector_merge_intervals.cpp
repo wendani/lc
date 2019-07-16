2806356ec669a64fcd1d89689dc10de5f9c4ef95
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-16 01:14:11 -0700

    On branch 56_vector_merge_intervals new file:   vector_merge_intervals.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ vector_merge_intervals.cpp
@@ -0,0 +1,23 @@
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+	vector<vector<int>> mergedIntervals;
+
+	int len = intervals.size();
+	if (！len) {
+		return mergedIntervals;
+	}
+
+	// reach here when intervals has at least one interval
+	int i = 0;
+	int close = intervals[i][1];
+	for (int j = i + 1; j < len; j++) {
+		if (close < intervals[j][0]) {
+			mergedIntervals.emplace_back(initializer_list<int>{intervals[i][0], close});
+			i = j;
+			close = intervals[i][1];
+		}
+		else if (close < intervals[j][1]) {
+			close = intervals[j][1];
+		}
+	}
+	mergedIntervals.emplace_back(initializer_list<int>{intervals[i][0], close});
+}
