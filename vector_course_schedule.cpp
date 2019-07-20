fb86569e50b7827ea79f16a06dfbd27a62fc5b96
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-20 14:36:52 -0700

    On branch 207_vector_course_schedule, use bfs approach
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- vector_course_schedule.cpp
+++ vector_course_schedule.cpp
@@ -1,3 +1,40 @@
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+	vector<vector<int>> adjList(numCourses);
+	vector<int> preCourseNum(numCourses, 0);
+
+	queue<int> zeroPreCourses;
+
+	for (const auto &prerequisite : prerequisites) {
+		adjList[prerequisite[1]].push_back(prerequisite[0]);
+		++preCourseNum[prerequisite[0]];
+	}
+
+	for (int i = 0; i < numCourses; i++) {
+		if (!preCourseNum[i]) {
+			zeroPreCourses.push(i);
+		}
+	}
+
+	int coursesTaken = 0;
+	while (!zeroPreCourses.empty()) {
+		int &i = zeroPreCourses.front();
+
+		for (const auto &j : adjList[i]) {
+			preCourseNum[j]--;
+			if (!preCourseNum[j]) {
+				zeroPreCourses.push(j);
+			}
+		}
+
+		++coursesTaken;
+		zeroPreCourses.pop();
+	}
+
+	return coursesTaken == numCourses;
+}
+
+
+
 bool  _dfs(const vector<vector<int>> &adjList, vector<bool> &searched, int courseNum)
 {
 	if (searched[courseNum]) {
