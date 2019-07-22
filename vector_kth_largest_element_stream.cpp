7b126ca67635a009253723da0582b27d7adc922a
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-21 20:18:50 -0700

    On branch 703_vector_kth_largest_element_stream new file:   vector_kth_largest_element_stream.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ vector_kth_largest_element_stream.cpp
@@ -0,0 +1,34 @@
+class KthLargest {
+public:
+	KthLargest(int k, vector<int>& nums) {
+		int i = 0;
+		for (; i < k; i++) {
+			minPriQ.push(nums[i]);
+		}
+
+		int len = nums.size();
+		for (i < len; i++) {
+			if (minPriQ.top() < nums[i]) {
+				minPriQ.pop();
+				minPriQ.push(nums[i]);
+			}
+		}
+	}
+
+	int add(int val) {
+		if (minPriQ.top() < nums[i]) {
+			minPriQ.pop();
+			minPriQ.push(nums[i]);
+		}
+		return minPriQ.top();
+	}
+
+private:
+	priority_queue<int, vector<int>, greater<int>> minPriQ;
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
