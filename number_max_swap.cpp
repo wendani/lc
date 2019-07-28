30caebbae9b402357403b28cd2c9a34a86de0089
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-28 00:11:58 -0700

    On branch 670_number_maximum_swap, fix logic
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- number_max_swap.cpp
+++ number_max_swap.cpp
@@ -10,7 +10,35 @@
  * Input: 9973
  * Output: 9973
  * Explanation: No swap.
+ *
+ *
+ * Input: 1993
+ * Output: 9193
+ * Expected: 9913
+ *
+ *
+ * Input: 98368
+ * Output: 98368
+ * Expected: 98863
  */
+
+static bool comp(const vector<int> &i, const vector<int> &j)
+{
+	if (i[0] < j[0]) {
+		return true;
+	}
+	else if (i[0] > j[0]) {
+		return false;
+	}
+
+	// reach here when i[0] == j[0]
+	if (i[1] < j[1]) {
+		return true;
+	}
+
+	assert(i[1] > j[1]);
+	return false;
+}
 
 class Solution {
 public:
@@ -29,12 +57,17 @@ public:
 		for (int i = 0; i < len; i++) {
 			dis.emplace_back(initializer_list<int>{digits[i], i});
 		}
-		sort(dis.begin(), dis.end());
+		sort(dis.begin(), dis.end(), comp);
 
 		for (int i = len - 1; i >= 0; i--) {
 			if (dis[i][1] != i) {
-				digits[dis[i][1]] = digits[i];
-				digits[i] = dis[i][0];
+				int j = i;
+				while (j - 1 >= 0 && dis[j - 1][0] == dis[i][0]) {
+					j--;
+				}
+
+				digits[dis[j][1]] = digits[i];
+				digits[i] = dis[j][0];
 				break;
 			}
 		}
@@ -50,23 +83,5 @@ public:
 
 private:
 	vector<int> digits;
-
-	bool comp(int i, int j)
-	{
-		if (digits[i] < digits[j]) {
-			return true;
-		}
-		else if (digits[i] > digits[j]) {
-			return false;
-		}
-
-		// reach here when digits[i] == digits[j]
-		if (i < j) {
-			return true;
-		}
-
-		assert(i > j);
-		return false;
-	}
 };
 
