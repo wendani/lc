7d45844a5e02874f8a3ebae4801dcf44701999e5
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-07-31 22:56:03 -0700

    On branch 31_vector_next_permutation, fix compile error
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- vector_next_permutation.cpp
+++ vector_next_permutation.cpp
@@ -17,7 +17,7 @@ public:
 	void nextPermutation(vector<int>& nums) {
 		int len = nums.size();
 
-		lastNum = nums[len - 1];
+		int lastNum = nums[len - 1];
 		for (int i = len - 2; i >= 0; i--) {
 			if (nums[i] < lastNum) {
 				sort(nums.begin() + i, nums.end(), greater<int>());
