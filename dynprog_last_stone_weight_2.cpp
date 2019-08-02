77511583d6bc541bc0e5c7a0a19c7f59c5a3a5df
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-08-01 19:33:57 -0700

    On branch 1049_dp_last_stone_weight_2
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- dynprog_last_stone_weight_2.cpp
+++ dynprog_last_stone_weight_2.cpp
@@ -10,6 +10,48 @@
  * we can combine 1 and 1 to get 0 so the array converts to [1] then that's the optimal value.
  */
 
+
+class Solution {
+public:
+	int lastStoneWeightII(vector<int>& stones) {
+		int weightSum = 0;
+		for (const auto &stone : stones) {
+			weightSum += stone;
+		}
+		int weightLimit = weightSum >> 1;
+
+		vector<vector<bool>> stoneWeight;
+		int len = stones.size();
+		for (int i = 0; i < len; i++) {
+			stoneWeight.emplace_back(weightLimit + 1, false);
+			stoneWeight[i][0] = true;
+		}
+
+		int maxWeight = stones[0];
+		stoneWeight[0][stones[0]] = true;
+		for (int i = 1; i < len; i++) {
+			int j;
+			for (j = 1; j < stones[i]; j++) {
+				if (stoneWeight[i - 1][j]) {
+					stoneWeight[i][j] = true;
+				}
+			}
+			for (j = stones[i]; j <= weightLimit; j++) {
+				if (stoneWeight[i - 1][j - stones[i]] || stoneWeight[i - 1][j]) {
+					stoneWeight[i][j] == true;
+					if (maxWeight < j) {
+						maxWeight = j;
+					}
+				}
+			}
+		}
+
+		int last = weightSum - maxWeight;
+		return last > maxWeight ? last - maxWeight : maxWeight - last;
+	}
+};
+
+
 class Solution {
 public:
 	int lastStoneWeightII(vector<int>& stones) {
