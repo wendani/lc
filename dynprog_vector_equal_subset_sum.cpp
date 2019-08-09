ad7c65eaad8b47b9d5415b0756e7b595b89d0442
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-08-09 00:49:21 -0700

    On branch 416_dp_partition_equal_subset_sum, optimize
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- dynprog_vector_equal_subset_sum.cpp
+++ dynprog_vector_equal_subset_sum.cpp
@@ -22,20 +22,15 @@ public:
 		}
 
 		int sumTarget = sum >> 1;
-		int len = nums.size();
-		for (int i = 0; i < len; i++) {
-			if (nums[i] == sumTarget) {
-				return true;
-			}
-			else if (nums[i] > sumTarget) {
-				return false;
-			}
-		}
-
 		vector<bool> numSum(sumTarget + 1, false);
+		int len = nums.size();
 		numSum[0] = true;
 		numSum[nums[0]] = true;
 		for (int i = 1; i < len; i++) {
+			if (nums[i] > sumTarget) {
+				return false;
+			}
+
 			for (int j = sumTarget; j >= nums[i]; j--) {
 				if (!numSum[j] && numSum[j - nums[i]]) {
 					if (j == sumTarget) {
