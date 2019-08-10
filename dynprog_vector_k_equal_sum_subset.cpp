cb24fe41b7b6faa567444a81aeab884ec8935f78
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-08-09 20:47:16 -0700

    On branch 698_dp_partition_k_equal_sum_subsets new file:   dynprog_vector_k_equal_sum_subset.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ dynprog_vector_k_equal_sum_subset.cpp
@@ -0,0 +1,45 @@
+/*
+ * Example 1:
+ *
+ * Input: nums = [4, 3, 2, 3, 5, 2, 1], k = 4
+ * Output: True
+ * Explanation: It's possible to divide it into 4 subsets (5), (1, 4), (2,3), (2,3) with equal sums.
+ */
+
+
+class Solution {
+public:
+	bool canPartitionKSubsets(vector<int>& nums, int k) {
+		int sum = 0;
+		for (const auto &num : nums) {
+			sum += num;
+		}
+
+		if (sum % k) {
+			return false;
+		}
+
+		int sumTarget = sum / k;
+		vector<bool> numSum(sumTarget + 1, false);
+		numSum[0] = true;
+		int len = nums.size();
+		for (int i = 0; i < len; i++) {
+			if (nums[i] > sumTarget) {
+				return false;
+			}
+
+			for (int j = sumTarget; j >= nums[i]; j--) {
+				if (!numSum[j] && numSum[j - nums[i]]) {
+					numSum[j] = true;
+				}
+			}
+		}
+
+		for (int i = sumTarget; i <= sum; i += sumTarget) {
+			if (!numSum[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
