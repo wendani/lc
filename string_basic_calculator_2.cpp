aafebf5557756b1ae2e308647c7f7afe211e607c
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-08-17 01:52:27 -0700

    On branch 227_string_basic_calculator_2 new file:   string_basic_calculator_2.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ string_basic_calculator_2.cpp
@@ -0,0 +1,86 @@
+/*
+ * Example 1:
+ *
+ * Input: "3+2*2"
+ * Output: 7
+ *
+ * Example 2:
+ *
+ * Input: " 3/2 "
+ * Output: 1
+ *
+ * Example 3:
+ *
+ * Input: " 3+5 / 2 "
+ * Output: 5
+ */
+
+class Solution {
+public:
+	int calculate(string s) {
+		stack<int> numStk;
+		stack<char> opStk;
+
+		int len = s.length();
+		int i = 0;
+		while (i < len) {
+			if (isDigit(s[i])) {
+				int num = 0;
+				while (i < len && isDigit(s[i])) {
+					num *= 10;
+					num += (s[i] - 48);
+					i++;
+				}
+				numStk.push(num);
+			}
+			else if (s[i] == '+' || s[i] == '-') {
+				opStk.push(s[i]);
+				i++;
+			}
+			else {
+				assert(s[i] == '*' || s[i] == '/');
+				char &op = s[i];
+
+				i++;
+				int numB = 0;
+				while (i < len && isDigit(s[i])) {
+					numB *= 10;
+					numB += (s[i] - 48);
+					i++;
+				}
+
+				int &numA = numStk.top();
+				if (op == '*') {
+					numA *= numB;
+				} else {
+					// op == '/'
+					numA /= numB;
+				}
+			}
+		}
+
+		int num = numStk.top();
+		numStk.pop();
+		while(!numStk.empty()) {
+			int &n = numStk.top();
+			if (opStk.top() == '-') {
+				num = n - num;
+			}
+			else {
+				num += n;
+			}
+
+			numStk.pop();
+			opStk.pop();
+		}
+		return num;
+	}
+private:
+	bool isDigit(const char &c)
+	{
+		if ('0' <= c && c <= '9') {
+			return true;
+		}
+		return false;
+	}
+};
