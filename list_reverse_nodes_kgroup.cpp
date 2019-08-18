c73f47fc0f0046292a516f2d11c6e75e5f20aac6
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-08-18 15:37:21 -0700

    On branch 25_list_reverse_nodes_kgroup new file:   list_reverse_nodes_kgroup.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ list_reverse_nodes_kgroup.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* reverseKGroup(ListNode* head, int k) {
+		stack<ListNode *> nodeStk;
+
+		ListNode **prevTail = &head;
+		while (*prevTail) {
+			// find a segment of k nodes
+			ListNode *curr = *prevTail;
+			int i = 0;
+			while (curr & i < k) {
+				nodeStk.push(curr);
+
+				curr = curr->next;
+				i++;
+			}
+			if (i < k) {
+				return head;
+			}
+
+			// reach here when we have a complete segment
+			ListNode *prev = NULL;
+			while (!nodeStk.empty()) {
+				if (!prev) {
+					prev = nodeStk.top();
+					*prevTail = prev;
+				}
+				else {
+					prev->next = nodeStk.top();
+					prev = prev->next;
+				}
+
+				nodeStk.pop();
+			}
+
+			// stitching with the unexplored
+			prev->next = curr;
+			prevTail = &prev->next;
+		}
+		return head;
+	}
+};
+
+/*
+ * Example:
+ *
+ * Given this linked list: 1->2->3->4->5
+ *
+ * For k = 2, you should return: 2->1->4->3->5
+ *
+ * For k = 3, you should return: 3->2->1->4->5
+ */
