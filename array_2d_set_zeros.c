69586213f0b6b0fe37d91fb8c0b7871c8a272f53
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-08-31 00:55:54 -0700

    On branch 73_array_2d_set_zeros new file:   array_2d_set_zeros.c
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ array_2d_set_zeros.c
@@ -0,0 +1,73 @@
+void setZeroes(int** matrix, int matrixSize, int* matrixColSize){
+	if (!matrix || !*matrix || matrixSize < 1 || !matrixSize || *matrixColSize < 1) {
+		return;
+	}
+
+	for (int i = 1; i < matrixSize; i++) {
+		for (int j = 1; j < *matrixColSize; j++) {
+			if (!matrix[i][j]) {
+				matrix[i][0] = 0;
+				matrix[0][j] = 0;
+			}
+		}
+	}
+
+	for (int i = 1; i < matrixSize; i++) {
+		if (!matrix[i][0]) {
+			for (int j = 1; j < *matrixColSize; j++) {
+				matrix[i][j] = 0;
+			}
+		}
+	}
+
+	for (int j = 1; j < *matrixColSize; j++) {
+		if (!matrix[0][j]) {
+			for (int i = 1; i < matrixSize; i++) {
+				matrix[i][j] = 0;
+			}
+		}
+	}
+
+	if (!matrix[0][0]) {
+		for (int i = 1; i < matrixSize; i++) {
+			matrix[i][0] = 0;
+		}
+
+		for (int j = 1; j < *matrixColSize; i++) {
+			matrix[0][j] = 0;
+		}
+	}
+}
+
+
+/*
+ * Example 1:
+ *
+ * Input:
+ * [
+ *   [1,1,1],
+ *   [1,0,1],
+ *   [1,1,1]
+ * ]
+ * Output:
+ * [
+ *   [1,0,1],
+ *   [0,0,0],
+ *   [1,0,1]
+ * ]
+ *
+ * Example 2:
+ *
+ * Input:
+ * [
+ *   [0,1,2,0],
+ *   [3,4,5,2],
+ *   [1,3,1,5]
+ * ]
+ * Output:
+ * [
+ *   [0,0,0,0],
+ *   [0,4,5,0],
+ *   [0,3,1,0]
+ * ]
+ */
