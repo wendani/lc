89beb60d6e5b0e9645dc7a4634aec713c29e079a
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-08-31 01:44:32 -0700

    On branch 73_array_2d_set_zeros, fix logic
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- array_2d_set_zeros.c
+++ array_2d_set_zeros.c
@@ -3,6 +3,22 @@ void setZeroes(int** matrix, int matrixSize, int* matrixColSize){
 		return;
 	}
 
+	bool setFirstColumn = false;
+	for (int i = 0; i < matrixSize; i++) {
+		if (!matrix[i][0]) {
+			setFirstColumn = true;
+			break;
+		}
+	}
+
+	bool setFirstRow = false;
+	for (int j = 0; j < *matrixColSize; j++) {
+		if (!matrix[0][j]) {
+			setFirstRow = true;
+			break;
+		}
+	}
+
 	for (int i = 1; i < matrixSize; i++) {
 		for (int j = 1; j < *matrixColSize; j++) {
 			if (!matrix[i][j]) {
@@ -28,12 +44,14 @@ void setZeroes(int** matrix, int matrixSize, int* matrixColSize){
 		}
 	}
 
-	if (!matrix[0][0]) {
-		for (int i = 1; i < matrixSize; i++) {
+	if (setFirstColumn) {
+		for (int i = 0; i < matrixSize; i++) {
 			matrix[i][0] = 0;
 		}
+	}
 
-		for (int j = 1; j < *matrixColSize; j++) {
+	if (setFirstRow) {
+		for (int j = 0; j < *matrixColSize; j++) {
 			matrix[0][j] = 0;
 		}
 	}
