8a8f542a57ad6aab3f1b3d0e7e8afa0b1e04d8cb
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-09-07 01:32:54 -0700

    On branch 155_vector_min_stack new file:   vector_min_stack.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ vector_min_stack.cpp
@@ -0,0 +1,51 @@
+class MinStack {
+public:
+	/** initialize your data structure here. */
+	MinStack() {
+	}
+
+	void push(int x) {
+		stk.push_back(x);
+
+		if (!minElmtStk.empty()) {
+			int &currMin = minElmtStk.top();
+			if (currMin < x) {
+				minElmtStk.push_back(currMin);
+			}
+			else {
+				minElmtStk.push_back(x);
+			}
+		}
+		else {
+			minElmtStk.push_back(x);
+		}
+	}
+
+	void pop() {
+		if (!stk.empty()) {
+			stk.pop();
+			minElmtStk.pop();
+		}
+	}
+
+	int top() {
+		return stk.top();
+	}
+
+	int getMin() {
+		return minElmtStk.top();
+	}
+private:
+	stack<int> stk;
+	stack<int> minElmtStk;
+};
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
