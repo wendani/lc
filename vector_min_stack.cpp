27190a371734307c3ab4df57e441ae862b468e6a
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-09-07 12:21:17 -0700

    On branch 155_vector_min_stack, reduce operation on minElmntStack
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- vector_min_stack.cpp
+++ vector_min_stack.cpp
@@ -7,6 +7,42 @@ public:
 	void push(int x) {
 		stk.push(x);
 
+		if (minElmtStk.empty() || x <= minElmtStk.top()) {
+			minElmtStk.push(x);
+		}
+	}
+
+	void pop() {
+		if (!stk.empty()) {
+			if (stk.top() == minElmtStk.top() {
+				minElmtStk.pop();
+			}
+			stk.pop();
+		}
+	}
+
+	int top() {
+		return stk.top();
+	}
+
+	int getMin() {
+		return minElmtStk.top();
+	}
+
+private:
+	stack<int> stk;
+	stack<int> minElmtStk;
+};
+
+class MinStack {
+public:
+	/** initialize your data structure here. */
+	MinStack() {
+	}
+
+	void push(int x) {
+		stk.push(x);
+
 		if (!minElmtStk.empty()) {
 			int &currMin = minElmtStk.top();
 			if (currMin < x) {
@@ -35,6 +71,7 @@ public:
 	int getMin() {
 		return minElmtStk.top();
 	}
+
 private:
 	stack<int> stk;
 	stack<int> minElmtStk;
