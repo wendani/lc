52e174cb21f567cfa2f4655346a0cb820392b7fe
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-09-07 15:42:15 -0700

    On branch 551_string_attendance_record_1 new file:   string_attendance_record_1.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ string_attendance_record_1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+	bool checkRecord(string s) {
+		int ACnt = 0;
+		int contLCnt = 0;
+
+		for (const auto &c : s) {
+			if (c == 'A') {
+				contLCnt = 0;
+				ACnt++;
+				if (ACnt > 1) {
+					return false;
+				}
+			}
+			else (c == 'L') {
+				contLCnt++;
+				if (contLCnt > 2) {
+					return false;
+				}
+			}
+			else {
+				contLCnt = 0;
+			}
+		}
+		return true;
+	}
+};
+
+
+/*
+ * Example 1:
+ *
+ * Input: "PPALLP"
+ * Output: True
+ *
+ * Example 2:
+ *
+ * Input: "PPALLL"
+ * Output: False
+ */
