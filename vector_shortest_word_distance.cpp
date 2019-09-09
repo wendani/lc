ef126fe23fdcb6e9869552c88ad666a8e66ec65d
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-09-09 01:34:26 -0700

    On branch 243_string_shortest_word_distance new file:   vector_shortest_word_distance.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ vector_shortest_word_distance.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+	int shortestDistance(vector<string>& words, string word1, string word2) {
+		int len = words.size();
+		int word1Idx = len;
+		int word2Idx = len;
+		inr minDist = len;
+		for (int i = 0; i < len; i++) {
+			if (words[i] == word1) {
+				word1Idx = i;
+
+				int dist = abs(word1Idx - word2Idx);
+				if (dist < minDist) {
+					minDist = dist;
+				}
+			}
+			else if (words[i] == word2) {
+				word2Idx = i;
+
+				int dist = abs(word1Idx - word2Idx);
+				if (dist < minDist) {
+					minDist = dist;
+				}
+			}
+		}
+		assert(minDist < len);
+		return minDist;
+	}
+};
+
+
+/*
+ * Example:
+ * Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+ *
+ * Input: word1 = “coding”, word2 = “practice”
+ * Output: 3
+ *
+ * Input: word1 = "makes", word2 = "coding"
+ * Output: 1
+ */
