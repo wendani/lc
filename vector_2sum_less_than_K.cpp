837f0bc5e5818a1e2bae2dcb3541b356204bf27f
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-09-14 18:50:02 -0700

    On branch 1099_vector_two_sum_less_than_K new file:   vector_2sum_less_than_K.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ vector_2sum_less_than_K.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+	int twoSumLessThanK(vector<int>& A, int K) {
+		int len = A.size();
+		if (len < 2) {
+			return -1;
+		}
+
+		sort(A.begin(), B.end());
+		int i = 0;
+		int j = len - 1;
+		int max = -1;
+		while (i < j) {
+			int sum = A[i] + A[j];
+			if (sum >= K) {
+				j--;
+			}
+			else {
+				// sum < K
+				if (sum > max) {
+					max = sum;
+				}
+				i++;
+			}
+
+		}
+		return max;
+	}
+};
+
+
+/*
+ * Example 1:
+ *
+ * Input: A = [34,23,1,24,75,33,54,8], K = 60
+ * Output: 58
+ * Explanation:
+ * We can use 34 and 24 to sum 58 which is less than 60.
+ *
+ * Example 2:
+ *
+ * Input: A = [10,20,30], K = 15
+ * Output: -1
+ * Explanation:
+ * In this case it's not possible to get a pair sum less that 15.
+ */
