cc3e1a1273272079f0d07f5b32741434f45259e6
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-09-19 01:18:29 -0700

    On branch 7_number_reverse_integer new file:   number_reverse_integer.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ number_reverse_integer.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+	int reverse(int x) {
+		bool isNegative = false;
+		if (x < 0) {
+			x = -x;
+			isNegative = true;
+		}
+		int xReverse = 0;
+		while (x) {
+			int rem = x % 10;
+			xReverse *= 10;
+			xReverse += rem;
+
+			x /= 10;
+		}
+		return isNegative ? -xReverse : xReverse;
+	}
+};
+
+
+/*
+ * @lc app=leetcode id=7 lang=cpp
+ *
+ * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (25.49%)
+ * Total Accepted:    805.5K
+ * Total Submissions: 3.2M
+ * Testcase Example:  '123'
+ *
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 123
+ * Output: 321
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: -123
+ * Output: -321
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: 120
+ * Output: 21
+ *
+ *
+ * Note:
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose
+ * of this problem, assume that your function returns 0 when the reversed
+ * integer overflows.
+ *
+ */
