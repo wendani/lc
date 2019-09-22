63e624a2ab4a52be2204564d28b6cfd4b3dfd9b9
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-09-22 01:14:19 -0700

    On branch 136_number_single_number new file:   number_single_number.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ number_single_number.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int singleNum = 0;
+		for (const auto &num : nums) {
+			singleNum ^= num;
+		}
+		return singleNum;
+	}
+};
+
+
+/*
+ * @lc app=leetcode id=136 lang=cpp
+ *
+ * [136] Single Number
+ *
+ * https://leetcode.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (61.46%)
+ * Total Accepted:    531.1K
+ * Total Submissions: 863.7K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * Given a non-empty array of integers, every element appears twice except for
+ * one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement
+ * it without using extra memory?
+ *
+ * Example 1:
+ *
+ *
+ * Input: [2,2,1]
+ * Output: 1
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ *
+ *
+ */
