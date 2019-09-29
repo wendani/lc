8744eef5d08fe68d7b36c956af6a6281e3ac4f63
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-09-29 02:04:16 -0700

    On branch 22_vector_parentheses_generate new file:   vector_parentheses_generate.cpp
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ vector_parentheses_generate.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+	vector<string> generateParenthesis(int n) {
+		vector<string> parentheses;
+		_generateParenthesis(parentheses, 0, n);
+		return parentheses;
+	}
+private:
+	void _generateParenthesis(vector<string> &parentheses, int closeParenthesisCredits, int openParenthesisNum)
+	{
+		if (closeParenthesisCredits == 0 && openParenthesisNum == 0) {
+			parentheses.push_back("");
+		}
+
+		for (int i = 1; i <= openParenthesisNum; i++) {
+			string preOpen(i, '(');
+
+			for (int j = i + closeParenthesisCredits; j > 0; j--) {
+				string preClose(j, ')');
+
+				vector<string> post;
+				_generateParenthesis(post, i - j + closeParenthesisCredits, openParenthesisNum - i);
+
+				for (const auto &pst : post) {
+					parentheses.push_back(preOpen + preClose + pst);
+				}
+			}
+		}
+	}
+};
+
+
+/*
+ * @lc app=leetcode id=22 lang=cpp
+ *
+ * [22] Generate Parentheses
+ *
+ * https://leetcode.com/problems/generate-parentheses/description/
+ *
+ * algorithms
+ * Medium (57.04%)
+ * Total Accepted:    397.6K
+ * Total Submissions: 694.8K
+ * Testcase Example:  '3'
+ *
+ *
+ * Given n pairs of parentheses, write a function to generate all combinations
+ * of well-formed parentheses.
+ *
+ *
+ *
+ * For example, given n = 3, a solution set is:
+ *
+ *
+ * [
+ * ⁠ "((()))",
+ * ⁠ "(()())",
+ * ⁠ "(())()",
+ * ⁠ "()(())",
+ * ⁠ "()()()"
+ * ]
+ *
+ */
