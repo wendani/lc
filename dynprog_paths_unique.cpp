101aa5065cb76b70dcd02358042181c999773cfb
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-09-29 19:51:04 -0700

    On branch 62_dp_paths_unique, fix corner case
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- dynprog_paths_unique.cpp
+++ dynprog_paths_unique.cpp
@@ -6,7 +6,7 @@ public:
 		}
 
 		vector<vector<int>> pathNum(m, vector<int>(n, 0));
-		for (int j = 0; j < n - 1; j++) {
+		for (int j = 0; j < n; j++) {
 			pathNum[m - 1][j] = 1;
 		}
 		for (int i = 0; i < m - 1; i++) {
