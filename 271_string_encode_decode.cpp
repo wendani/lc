7d988a538e50a58868bc6120828b62ba885d8ea5
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2026-07-10 17:19:00 -0700

    Remove
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- 271_string_encode_decode.cpp
+++ 271_string_encode_decode.cpp
@@ -49,10 +49,7 @@ public:
 
 /*
  * 271. Encode and Decode Strings
- * Medium
- * Topics
- * conpanies icon
- * Companies
+ *
  * Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
  *
  * Machine 1 (sender) has the function:
