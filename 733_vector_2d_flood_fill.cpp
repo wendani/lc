da73de5418a7414d5e73404b177ddb65258f0255
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2026-07-11 17:46:52 -0700

    733 init impl
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ 733_vector_2d_flood_fill.cpp
@@ -0,0 +1,132 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int rowLen = image.size();
+        int columnLen = image[0].size();
+
+        unordered_set<int> coloredPixels;
+        queue<int> unexploredPixels;
+
+        int oldColor = image[sr][sc];
+        if (oldColor != color)
+        {
+            image[sr][sc] = color;
+
+            int id = sr * columnLen + sc;
+            coloredPixels.emplace(id);
+            unexploredPixels.push(id);
+        }
+
+        while (!unexploredPixels.empty())
+        {
+            const int &id = unexploredPixels.front();
+
+            int i = id / columnLen;
+            int j = id % columnLen;
+
+            if (j - 1 >= 0 && !coloredPixels.count(id - 1))
+            {
+                // Pixel with origin color
+                if (image[i][j - 1] == oldColor)
+                {
+                    image[i][j - 1] = color;
+
+                    coloredPixels.emplace(id - 1);
+                    unexploredPixels.push(id - 1);
+                }
+            }
+            if (j + 1 < columnLen && !coloredPixels.count(id + 1))
+            {
+                // Pixel with origin color
+                if (image[i][j + 1] == oldColor)
+                {
+                    image[i][j + 1] = color;
+
+                    coloredPixels.emplace(id + 1);
+                    unexploredPixels.push(id + 1);
+                }
+            }
+            if (i - 1 >= 0 && !coloredPixels.count(id - columnLen))
+            {
+                // Pixel with origin color
+                if (image[i - 1][j] == oldColor)
+                {
+                    image[i - 1][j] = color;
+
+                    coloredPixels.emplace(id - columnLen);
+                    unexploredPixels.push(id - columnLen);
+                }
+            }
+            if (i + 1 < rowLen && !coloredPixels.count(id + columnLen))
+            {
+                // Pixel with origin color
+                if (image[i + 1][j] == oldColor)
+                {
+                    image[i + 1][j] = color;
+
+                    coloredPixels.emplace(id + columnLen);
+                    unexploredPixels.push(id + columnLen);
+                }
+            }
+
+            unexploredPixels.pop();
+        }
+
+        return image;
+    }
+};
+
+/*
+ * 733. Flood Fill
+ * Easy
+ * Topics
+ * conpanies icon
+ * Companies
+ * Hint
+ * You are given an image represented by an m x n grid of integers image, where image[i][j] represents the pixel value of the image. You are also given three integers sr, sc, and color. Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].
+ *
+ * To perform a flood fill:
+ *
+ * Begin with the starting pixel and change its color to color.
+ * Perform the same process for each pixel that is directly adjacent (pixels that share a side with the original pixel, either horizontally or vertically) and shares the same color as the starting pixel.
+ * Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color if it matches the original color of the starting pixel.
+ * The process stops when there are no more adjacent pixels of the original color to update.
+ * Return the modified image after performing the flood fill.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+ *
+ * Output: [[2,2,2],[2,2,0],[2,0,1]]
+ *
+ * Explanation:
+ *
+ *
+ *
+ * From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+ *
+ * Note the bottom corner is not colored 2, because it is not horizontally or vertically connected to the starting pixel.
+ *
+ * Example 2:
+ *
+ * Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+ *
+ * Output: [[0,0,0],[0,0,0]]
+ *
+ * Explanation:
+ *
+ * The starting pixel is already colored with 0, which is the same as the target color. Therefore, no changes are made to the image.
+ *
+ *
+ *
+ * Constraints:
+ *
+ * m == image.length
+ * n == image[i].length
+ * 1 <= m, n <= 50
+ * 0 <= image[i][j], color < 216
+ * 0 <= sr < m
+ * 0 <= sc < n
+ */
