5a6efd26d9e88cb35131bf900f419b1f14911aca
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2026-07-19 01:01:54 -0700

    2408
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ 2408_class_design_sql.cpp
@@ -0,0 +1,31 @@
+class SQL {
+public:
+    SQL(vector<string>& names, vector<int>& columns) {
+
+    }
+
+    bool ins(string name, vector<string> row) {
+
+    }
+
+    void rmv(string name, int rowId) {
+
+    }
+
+    string sel(string name, int rowId, int columnId) {
+
+    }
+
+    vector<string> exp(string name) {
+
+    }
+};
+
+/**
+ * Your SQL object will be instantiated and called as such:
+ * SQL* obj = new SQL(names, columns);
+ * bool param_1 = obj->ins(name,row);
+ * obj->rmv(name,rowId);
+ * string param_3 = obj->sel(name,rowId,columnId);
+ * vector<string> param_4 = obj->exp(name);
+ */
