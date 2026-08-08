b9fb65878f49c62ed5c167031c88008e80596d56
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2026-08-07 23:01:07 -0700

    init
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

--- 419_vector_2d_battleship.cpp
+++ 419_vector_2d_battleship.cpp
@@ -1,6 +1,52 @@
 class Solution {
 public:
     int countBattleships(vector<vector<char>>& board) {
+        int rowLen = board.size();
+        int columnLen = board[0].size();
 
+        int count = 0;
+        for (int i = 0; i < rowLen; i++)
+        {
+            for (int j = 0; j < columnLen; j++)
+            {
+                if (board[i][j] == 'X')
+                {
+                    count++;
+
+                    battleship(board, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+private:
+    void battleship(vector<vector<int>> &board, const int i, const int j)
+    {
+        if (board[i][j] != 'X')
+        {
+            return;
+        }
+
+        // board[i][j] == 'X' when we reach here
+        board[i][j] = 'Z';
+
+        if (j - 1 >= 0)
+        {
+            battleship(board, i, j - 1);
+        }
+        if (j + 1 < columnLen)
+        {
+            battleship(board, i, j + 1);
+        }
+        if (i - 1 >= 0)
+        {
+            battleship(board, i - 1, j);
+        }
+        if (i + 1 < rowLen)
+        {
+            battleship(board, i + 1, j);
+        }
     }
 };
